6387f484ba23f59842b2c875f8dee88ec7220067
dakriy <git@persignum.com>
2018-12-06 09:38:58 -0800

    Lastest changes after the test last weekend.

---
+++ Shutdown/listen-for-shutdown.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO
+import subprocess
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.wait_for_edge(4, GPIO.FALLING)
+
+subprocess.call(['shutdown', '-h', 'now'], shell=False)
---
+++ Video/record-video.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import cv2
+import time
+import signal
+
+class GracefulKiller:
+    kill_now = False
+    def __init(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+        signal.signal(signal.SIGKILL, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        self.kill_now = True
+
+start = time.time()
+framerate = 10
+fourcc = "DIVX"
+cap = cv2.VideoCapture(0)
+file = "/home/pi/Measurements/Video/zucc " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start)) + ".avi"
+resolution = (640, 480)
+
+if cap.isOpened() == False:
+    print("Unable to access camera")
+    exit(1)
+
+write = cv2.VideoWriter_fourcc(*fourcc)
+out = cv2.VideoWriter(file, write, framerate, resolution)
+killer = GracefulKiller()
+
+while True:
+    ret, frame = cap.read()
+    if ret and not killer.kill_now:
+        out.write(frame)
+    else:
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
