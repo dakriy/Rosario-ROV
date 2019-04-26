99ad7f019a51b53628b8dd6772c85737934d9d15
dakriy <git@persignum.com>
2019-04-25 17:03:46 -0700

    Use tabs in some files instead of 4 spaces

--- ROVController/src/Computation/Token.h
+++ ROVController/src/Computation/Token.h
@@ -1,90 +1,90 @@
 #pragma once
 namespace Computation {
-    enum class TokenType {
-        Operator,
-        Function,
-        Number,
-        X,
-        Y
-    };
+	enum class TokenType {
+		Operator,
+		Function,
+		Number,
+		X,
+		Y
+	};
 
-    enum class Operator {
-        Add,
-        Subtract,
-        Divide,
-        Multiply,
-        Power,
-        Modulus,
-        Equals,
-        Negate
-    };
+	enum class Operator {
+		Add,
+		Subtract,
+		Divide,
+		Multiply,
+		Power,
+		Modulus,
+		Equals,
+		Negate
+	};
 
-    enum class Function {
-        Logarithm,
-        Sin,
-        Cos,
-        Tan,
-        Abs,
-        Exp,
-        Ln,
-        Log10,
-        Sqrt,
-        Pi,
-        E,
-        Step,
-        Max,
-        Min,
-        Sign,
-        Floor,
-        Ceil,
-        Atan,
-        Acos,
-        Asin,
-        Cosh,
-        Tanh,
-        Sinh
-    };
+	enum class Function {
+		Logarithm,
+		Sin,
+		Cos,
+		Tan,
+		Abs,
+		Exp,
+		Ln,
+		Log10,
+		Sqrt,
+		Pi,
+		E,
+		Step,
+		Max,
+		Min,
+		Sign,
+		Floor,
+		Ceil,
+		Atan,
+		Acos,
+		Asin,
+		Cosh,
+		Tanh,
+		Sinh
+	};
 
-    class Token {
-    protected:
-        static constexpr auto zero = 0.000000001;
+	class Token {
+	protected:
+		static constexpr auto zero = 0.000000001;
 
-        union value {
-            Operator o;
-            Function f;
-            double n;
-        };
+		union value {
+			Operator o;
+			Function f;
+			double n;
+		};
 
-        value v;
+		value v;
 
-        TokenType t;
+		TokenType t;
 
-        Token * left = nullptr;
-        Token * right = nullptr;
+		Token * left = nullptr;
+		Token * right = nullptr;
 
-        double operate(double x, double y);
-        double exec(double x, double y);
-    public:
-        Token();
-        // Performs deep copy
-        Token(Token &token);
-        explicit Token(double n, Token *l = nullptr, Token *r = nullptr);
-        explicit Token(Operator o, Token *l = nullptr, Token *r = nullptr);
-        explicit Token(Function f, Token *l = nullptr, Token *r = nullptr);
-        double compute(double x, double y);
-        TokenType getType();
-        void setOperator(Operator o);
-        void setFunction(Function f);
-        void setNumber(double n);
-        void setX();
-        void setY();
-        void setLeft(Token * l);
-        void setRight(Token * r);
-        bool isZero() const;
-        bool addToken(Token * t);
-        static void deepCopyNode(Token * original, Token * &copyTo);
-        Token * getLeft();
-        Token * getRight();
-        ~Token();
-    };
+		double operate(double x, double y);
+		double exec(double x, double y);
+	public:
+		Token();
+		// Performs deep copy
+		Token(Token &token);
+		explicit Token(double n, Token *l = nullptr, Token *r = nullptr);
+		explicit Token(Operator o, Token *l = nullptr, Token *r = nullptr);
+		explicit Token(Function f, Token *l = nullptr, Token *r = nullptr);
+		double compute(double x, double y);
+		TokenType getType();
+		void setOperator(Operator o);
+		void setFunction(Function f);
+		void setNumber(double n);
+		void setX();
+		void setY();
+		void setLeft(Token * l);
+		void setRight(Token * r);
+		bool isZero() const;
+		bool addToken(Token * t);
+		static void deepCopyNode(Token * original, Token * &copyTo);
+		Token * getLeft();
+		Token * getRight();
+		~Token();
+	};
 }
